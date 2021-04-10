82a61dd7aef52d6aff207aabf258e304f0153b5e
Pavan Datta Abbineni <apd10@iitbbs.ac.in>
2021-04-10 12:09:30 +0530

    ~

---
+++ prac.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+# plt.scatter(0,0)
+# # plt.text(0,0,'a')
+# # plt.show()
+#
+# for i in range(1000):
+#     theta = np.random.uniform(0, 2*3.14)                              ##randomly allocating the base station
+#     dist = np.random.uniform(19, 19)
+#     x = dist * math.cos(theta)
+#     y = dist * math.sin(theta)
+#     plt.scatter(x,y)
+#
+# plt.show()
+a=10
+b=20
+for i in range(a,a+b):
+    print(i)
